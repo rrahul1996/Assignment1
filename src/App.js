69978f9bded2f6f83404ed947ef66686b7c18e93
Ramakant Pawar <ramakantpawar36@gmail.com>
2022-06-21 19:00:22 +0530

    assignment1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [total, setTotal] = useState(number1 + number2);
+  const [substract, setSubstract] = useState(number1 + number2);
+  const [multiplication, setMultiplication] = useState(number1 + number2);
+  const [division, setDivision] = useState(number1 + number2);
+
+  const addHandle = () => {
+    setTotal(parseInt(number1) + parseInt(number2) )
+  }
+
+  const substractHandle = () => {
+    setSubstract(number1 - number2 )
+  }
+
+  const multiplicationHandle = () => {
+    setMultiplication(number1 * number2 )
+  }
+
+  const divisionHandle = () => {
+    setDivision(number1 / number2 )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='number_inputs'>
+        <div className='input1'>
+            <label>Number 1 :</label>
+            <input type="number" placeholder="0"
+             onChange={e => setNumber1(e.target.value)}/>
+        </div>
+        <div className='input1'>
+          <label>Number 2 :</label>
+          <input type="number" placeholder="0"
+          onChange={e => setNumber2(e.target.value)}
+          />
+        </div> 
+      </div>
+
+     {/* Buttons */}
+
+      <div className='buttonflex'>
+        <div className='buttons'>
+          <button type="button" class="btn btn-primary" onClick={addHandle}>Add</button>
+          <span className='value'>{total}</span>
+        </div>
+        <div className='buttons'>
+          <button type="button" class="btn btn-secondary" onClick={substractHandle}>Subtract</button>
+          <span className='value'>{substract}</span>
+        </div>
+        <div className='buttons'>
+          <button type="button" class="btn btn-success" onClick={multiplicationHandle}>Multiplication</button>
+          <span className='value'>{multiplication}</span>
+        </div> 
+        <div className='buttons'>
+          <button type="button" class="btn btn-danger" onClick={divisionHandle}>Division</button>
+          <span className='value'>{division}</span>
+        </div>  
+      </div>
+      
     </div>
   );
 }
